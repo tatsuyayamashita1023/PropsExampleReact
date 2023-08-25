a5f9489e81bbbefda0520298344a1ee4268c3948
tatsuyayamashita1023 <yamashita_tatsuya2@retail-ai.jp>
2023-08-25 16:46:56 +0900

    update Postsdata.jsx
    Posts.jsxにcommentOutを入れました

--- propsexample/src/components/Posts.jsx
+++ propsexample/src/components/Posts.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 
-export const Posts = ({post}) => {
+// const Posts = (post) => 　の()内に引数を書く
+//引数名は何の引数かわかる名前にしておけばなんでも良い(Ex.comment,itikome etc.)
+export const Posts = (post) => {
+
   return (
     <span>
       {post}
