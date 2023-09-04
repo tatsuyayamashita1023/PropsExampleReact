07dcbff912c594f460bff6db70e32850ec57daca
tatsuyayamashita1023 <yamashita_tatsuya2@retail-ai.jp>
2023-09-04 21:32:13 +0900

    Update Posts.jsx

---
+++ propsexample/src/components/Posts.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// const Posts = (post) => 　の()内に引数を書く
+//引数名は何の引数かわかる名前にしておけばなんでも良い(Ex.comment,itikome etc.)
+export const Posts = ({post}) => {
+
+  return (
+    <span>
+      {post}
+    </span>
+  )
+}
+
+
