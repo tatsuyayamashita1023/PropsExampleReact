15c6065625bf273913ae6caf6ac9cf46c7700e84
tatsuyayamashita1023 <yamashita_tatsuya2@retail-ai.jp>
2023-09-04 21:15:31 +0900

    Change Posts.jsx

--- propsexample/src/components/Posts.jsx
+++ propsexample/src/components/Posts.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 // const Posts = (post) => 　の()内に引数を書く
 //引数名は何の引数かわかる名前にしておけばなんでも良い(Ex.comment,itikome etc.)
-export const Posts = (post) => {
+export const Posts = ({post}) => {
 
   return (
     <span>
